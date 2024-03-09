8b6fca8d5792b7f947d24004c5f0f91801918b21
Martin Levesque <levesque.martin@gmail.com>
2024-03-09 18:18:21 -0500

    wip

--- lua/custom/plugins/custom-keybindings.lua
+++ lua/custom/plugins/custom-keybindings.lua
@@ -2,9 +2,12 @@
 
 local builtin = require 'telescope.builtin'
 vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('i', '<C-p>', "<Esc>:lua require('telescope.builtin').find_files()<CR>", { noremap = true })
 vim.keymap.set('n', '<C-f>', builtin.live_grep, { desc = '[F]ind in [F]iles' })
+vim.keymap.set('i', '<C-f>', "<Esc>:lua require('telescope.builtin').live_grep()<CR>", { noremap = true })
 
 vim.keymap.set('n', '<C-d>', '<cmd>Telescope dir live_grep<CR>', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-d>', '<Esc>:Telescope dir live_grep<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-s>', '<cmd>w<CR>', { desc = 'Save' })
 vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>a', { noremap = true, silent = true, desc = 'Save' })
 
